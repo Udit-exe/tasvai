97b07104ce82852368a4110f5fc9783bfc4698f4
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-07-17 07:53:08 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   client/src/App.jsx
    	new file:   client/src/pages/CreatePost.jsx
    	new file:   client/src/pages/Home.jsx
    	new file:   client/src/pages/index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {BrowserRouter , Link, Route, Routes} 
+from 'react-router-dom';
+
+import { logo } from './assets';
+import {Home , CreatePost} from './pages'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
+        {/*change logo in future */}
+        <Link to="/">
+          <img src={logo} alt="logo" className='w-28 object-contain'/>
+        </Link>
+
+        <Link to="/create-post" className='font-inter font-medium bg-[#24a0ed] text-white px-4 py-2 rounded-md'>
+          Create
+        </Link>
+
+        
+      </header>
+    </BrowserRouter>
   )
 }
 
