42bbd06207c900e39f6c0fda5bd03ba07fbf7f82
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-07-17 12:21:46 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   client/src/App.jsx
    	new file:   client/src/components/Card.jsx
    	new file:   client/src/components/FormField.jsx
    	new file:   client/src/components/Loader.jsx
    	new file:   client/src/components/index.js
    	modified:   client/src/pages/Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,24 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
+        
         {/*change logo in future */}
         <Link to="/">
           <img src={logo} alt="logo" className='w-28 object-contain'/>
         </Link>
 
-        <Link to="/create-post" className='font-inter font-medium bg-[#24a0ed] text-white px-4 py-2 rounded-md'>
+        <Link to="/create-post" className='font-inter font-medium bg-[#9EC8BE] text-white px-4 py-2 rounded-md'>
           Create
-        </Link>
-
         
+        </Link>
       </header>
+
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#202020] min-h-[calc (100vh - 73px)]'>
+        <Routes>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/create-post' element={ <CreatePost/> }/>
+        </Routes>
+      </main>
     </BrowserRouter>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
+
+import {Loader , Card, FormField} from "../components";
 
 const Home = () => {
+  const [loading, setloading] = useState(false);
+  const [allPosts, setallPosts] = useState(null);
+  const [searchText, setsearchText] = useState('Hello world');
+
   return (
-    <div>Home</div>
+    <section className='max-w-7x1 mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#EEEEEE] text-[32px]'>The GenAI Generation</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Try out this GenAI application for generating images</p>
+      </div>
+
+      <div className='mt-16'>
+        <FormField/>
+      </div>
+
+      <div className='mt-10'>
+        {loading ? (
+          <div className='flex justify-center items-center'>
+            <Loader/>
+          </div>
+        ): (
+          <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                Showing results for <span className='text-[#50727B]'>{searchText}</span>
+              </h2>
+            )}
+          </>
+        )}
+      </div>
+
+    </section>
   )
 }
 
