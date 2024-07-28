40d8bb9681a6e39ac06f6b8d34bee849d05c9d36
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-07-28 13:18:16 +0000

    modified:   server/index.js
     Untracked files:
    	.replit
    	server/mognodb/models/
    	server/routes/

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './mognodb/connect.js';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mognodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/' , async(req , res) => {
-    res.send("Hello from DALL-E!"); 
+app.use("api/v1/post", postRoutes);
+app.use("api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+    res.send("Hello from DALL-E!");
 });
 
-const startServer = async() =>{
+const startServer = async () => {
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080 , () => console.log('server started at port : 8080'))
+        app.listen(8080, () => console.log("server started at port : 8080"));
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 startServer();
